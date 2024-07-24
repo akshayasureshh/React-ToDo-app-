b867e4c1c0cece42911085e6a0c16cfa0f0807b5
akshayasureshh <akshayasuresh5421@gmail.com>
2024-07-24 22:05:36 +0530

    status check with id

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [toDos,setToDos] = useState([])
+  const [toDo,setToDo] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Lets Make A List</h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id:Date.now() ,text:toDo,status:false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        { toDos.map((value)=>{
+
+        return(
+        <div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+              console.log(e.target.value)
+              console.log(value)
+              setToDos(toDos.filter(value2=>{
+                if (value2.id === value.id){
+                  value2.status = e.target.checked
+                }
+                return value2
+              }))
+
+            }} value={value.status} type="checkbox" name="" id="" />
+            <p>{value.text}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>)
+})}
+      </div>
+
+      {
+        toDos.map((value)=>{
+          if(value.status){
+            return(<h1>{value.text}</h1>)
+          }
+          return null
+        })}
+     
     </div>
   );
 }
---
+++ src/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Counter(props) {
+  return (
+    <div >
+        <p>Counter:{props.count}</p>
+      
+    </div>
+  )
+}
+
+export default Counter
